aa9823ce25f6b383b31fae0e988c3705b29b4f20
EduardaDalmas <du.dalmas@hotmail.com>
2023-05-26 20:06:31 -0300

    first commit

---
+++ sextou/src/main/java/br/com/ienh/appsextou/sextou/SextouApplication.java
@@ -0,0 +1,13 @@
+package br.com.ienh.appsextou.sextou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SextouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SextouApplication.class, args);
+	}
+
+}
---
+++ sextou/src/test/java/br/com/ienh/appsextou/sextou/SextouApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ienh.appsextou.sextou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SextouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
