278d59f287a720d957f265170023d5114be5a080
EduardaDalmas <du.dalmas@hotmail.com>
2023-05-26 21:45:36 -0300

    params via url de 2 formas

--- sextou/src/main/java/br/com/ienh/appsextou/sextou/ProfessorController.java
+++ sextou/src/main/java/br/com/ienh/appsextou/sextou/ProfessorController.java
@@ -3,6 +3,7 @@ package br.com.ienh.appsextou.sextou;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,6 +22,15 @@ public class ProfessorController {
     public String teste2(@RequestParam String nome, Model model) {
         model.addAttribute("nome", nome);
         return "teste2";
+    }
+
+    @GetMapping("/teste3/{id}/{nome}")
+    public String teste3(@PathVariable int id, @PathVariable String nome, Model model) {
+        model.addAttribute("nome", nome);
+        model.addAttribute("id", id);
+        System.out.println(id);
+        System.out.println(nome);
+        return "teste3";
     }
 
 }
